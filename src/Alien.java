884acf908db724ba2f6915a55d42ea0e0d028f21
elija <elijah.gregory222@gmail.com>
2024-11-02 14:00:42 -0300

    first commit

---
+++ src/Alien.java
@@ -0,0 +1,24 @@
+/*.
+Input parameters: stressLevel (int), isWounded (boolean)
+Expected output: number of dice to roll (int)
+Write code that checks if a Panic Roll (1d6 + stress level)
+triggers panic effects. If stress level is over 10, add additional conditions.
+
+*/
+import java.util.Scanner;
+public class Alien {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your stress: ");
+        int stress = sc.nextInt();
+
+        System.out.println("Are you wounded. 1 for yes, 2 for no: ");
+        int wounded = sc.nextInt();
+
+        int totalDice = stress;
+
+        if ( totalDice >= 10) {
+            System.out.println ("You now have a additional condition.");}
+
+    }
+}
